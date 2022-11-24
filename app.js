404a7bdd9fae0a3ca3c2931fcacce01ba7a237bc
mosidrum <mosiokanga@gmail.com>
2022-11-24 12:55:00 +0100

    First step taken

---
+++ app.js
@@ -0,0 +1,9 @@
+const number = document.getElementById('number');
+const button = document.getElementById('generator');
+
+const randomNumberGenerator = () => {
+    const randomNumber = Math.random();
+
+    console.log(randomNumber)
+}
+randomNumberGenerator();
